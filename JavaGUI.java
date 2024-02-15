480b0fbcf0b90d161e8cc48ff2f2724111f41ad9
WDAhyhy <142432507+WDAhyhy@users.noreply.github.com>
2024-02-15 22:47:56 +0800

    完成插入

--- JavaGUI.java
+++ JavaGUI.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+
 //需要实现插入，删除，查找，添加，判断
 public class JavaGUI {
     //加载C库
@@ -66,7 +67,8 @@ public class JavaGUI {
 }
 class MyFrame extends JFrame{
     Container container;
-    JPanel panel_init,panel_insert;
+    MyPanel_insert myPanel_insert;
+    MyPanel_init myPanel_init;
 
     public  MyFrame(){
         super("族谱管理系统");
@@ -77,10 +79,10 @@ class MyFrame extends JFrame{
         this.container.setLayout(null);
         //面板
         //初始面板
-        this.panel_init=new MyPanel_init(this).panel_init;
+        this.myPanel_init=new MyPanel_init(this);
         //插入面板
-        this.panel_insert=new MyPanel_insert(this).panel_insert;
-        this.container.add(this.panel_init);
+        this.myPanel_insert=new MyPanel_insert(this);
+        this.container.add(this.myPanel_init.panel_init);
         pack();
 
     }
@@ -90,6 +92,10 @@ class MyPanel_insert extends JPanel{
     JPanel panel_insert;
     InsertButton insertButton;
     MyFrame frame=null;
+    JTextArea fatherJTextArea,nameJTextArea,byearJTextArea,bmonthJTextArea,bdayJTextArea,
+        addressJTextArea,dyearJTextArea,dmonthJTextArea,ddayJTextArea;
+    JRadioButton aliveYesJRadioButton,aliveNoJRadioButton;
+    ButtonGroup aliveButtonGroup;
     public MyPanel_insert(MyFrame frame){
         this.frame=frame;
         this.panel_insert=new JPanel();
@@ -99,38 +105,38 @@ class MyPanel_insert extends JPanel{
 
 
         JLabel fatherJLabel=new JLabel("父亲：");
-        JTextArea fatherJTextArea=new JTextArea(1,5);
+        this.fatherJTextArea=new JTextArea(1,5);
         fatherJLabel.setBounds(50,50,100,100);
         fatherJLabel.setFont(new Font("宋体",Font.BOLD,30));
-        fatherJTextArea.setBounds(150,85,100,30);
-        fatherJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
+        this.fatherJTextArea.setBounds(150,85,100,30);
+        this.fatherJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
 
 
         JLabel nameJLabel=new JLabel("姓名：");
-        JTextArea nameJTextArea=new JTextArea(1,5);
+        this.nameJTextArea=new JTextArea(1,5);
         nameJLabel.setBounds(50,150,100,100);
         nameJLabel.setFont(new Font("宋体",Font.BOLD,30));
-        nameJTextArea.setBounds(150,185,100,30);
-        nameJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
+        this.nameJTextArea.setBounds(150,185,100,30);
+        this.nameJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
 
         JLabel birthJLabel= new JLabel("生日：");
-        JTextArea byearJTextArea=new JTextArea(1,5);
+        this.byearJTextArea=new JTextArea(1,5);
         birthJLabel.setBounds(50,250,100,100);
         birthJLabel.setFont(new Font("宋体",Font.BOLD,30));
-        byearJTextArea.setBounds(150,285,100,30);
-        byearJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
+        this.byearJTextArea.setBounds(150,285,100,30);
+        this.byearJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
         JLabel byearJLabel=new JLabel("年");
         byearJLabel.setBounds(275,253,100,100);
         byearJLabel.setFont(new Font("宋体",Font.BOLD,30));
-        JTextArea bmonthJTextArea=new JTextArea(1,5);
-        bmonthJTextArea.setBounds(350,285,100,30);
-        bmonthJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
+        this.bmonthJTextArea=new JTextArea(1,5);
+        this.bmonthJTextArea.setBounds(350,285,100,30);
+        this.bmonthJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
         JLabel bmonthJLabel=new JLabel("月");
         bmonthJLabel.setBounds(475,253,100,100);
         bmonthJLabel.setFont(new Font("宋体",Font.BOLD,30));
-        JTextArea bdayJTextArea=new JTextArea(1,5);
-        bdayJTextArea.setBounds(550,285,100,30);
-        bdayJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
+        this.bdayJTextArea=new JTextArea(1,5);
+        this.bdayJTextArea.setBounds(550,285,100,30);
+        this.bdayJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
         JLabel bdayJLabel=new JLabel("日");
         bdayJLabel.setBounds(675,253,100,100);
         bdayJLabel.setFont(new Font("宋体",Font.BOLD,30));
@@ -138,42 +144,42 @@ class MyPanel_insert extends JPanel{
 
 
         JLabel addressJLabel= new JLabel("地址：");
-        JTextArea addressJTextArea= new JTextArea(1,20);
+        this.addressJTextArea= new JTextArea(1,20);
         addressJLabel.setBounds(50,350,100,100);
         addressJLabel.setFont(new Font("宋体",Font.BOLD,30));
-        addressJTextArea.setBounds(150,385,500,30);
-        addressJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
+        this.addressJTextArea.setBounds(150,385,500,30);
+        this.addressJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
 
         JLabel aliveJLabel=new JLabel("存活：");
-        JRadioButton aliveYesJRadioButton=new JRadioButton("是");
-        JRadioButton aliveNoJRadioButton=new JRadioButton("否");
-        ButtonGroup aliveButtonGroup=new ButtonGroup();
-        aliveButtonGroup.add(aliveNoJRadioButton);
-        aliveButtonGroup.add(aliveYesJRadioButton);
+        this.aliveYesJRadioButton=new JRadioButton("是");
+        this.aliveNoJRadioButton=new JRadioButton("否");
+        this.aliveButtonGroup=new ButtonGroup();
+        this.aliveButtonGroup.add(this.aliveNoJRadioButton);
+        this.aliveButtonGroup.add(this.aliveYesJRadioButton);
         aliveJLabel.setBounds(50,450,100,100);
         aliveJLabel.setFont(new Font("宋体",Font.BOLD,30));
-        aliveYesJRadioButton.setBounds(150,485,40,30);
-        aliveNoJRadioButton.setBounds(200,485,40,30);
+        this.aliveYesJRadioButton.setBounds(150,485,40,30);
+        this.aliveNoJRadioButton.setBounds(200,485,40,30);
 
 
         JLabel deathJLabel=new JLabel("死亡日期：");
-        JTextArea dyearJTextArea=new JTextArea(1,5);
+        this.dyearJTextArea=new JTextArea(1,5);
         deathJLabel.setBounds(50,550,200,100);
         deathJLabel.setFont(new Font("宋体",Font.BOLD,30));
-        dyearJTextArea.setBounds(200,585,100,30);
-        dyearJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
+        this.dyearJTextArea.setBounds(200,585,100,30);
+        this.dyearJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
         JLabel dyearJLabel=new JLabel("年");
         dyearJLabel.setBounds(325,553,100,100);
         dyearJLabel.setFont(new Font("宋体",Font.BOLD,30));
-        JTextArea dmonthJTextArea=new JTextArea(1,5);
-        dmonthJTextArea.setBounds(375,585,100,30);
-        dmonthJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
+        this.dmonthJTextArea=new JTextArea(1,5);
+        this.dmonthJTextArea.setBounds(375,585,100,30);
+        this.dmonthJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
         JLabel dmonthJLabel=new JLabel("月");
         dmonthJLabel.setBounds(500,553,100,100);
         dmonthJLabel.setFont(new Font("宋体",Font.BOLD,30));
-        JTextArea ddayJTextArea=new JTextArea(1,5);
-        ddayJTextArea.setBounds(550,585,100,30);
-        ddayJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
+        this.ddayJTextArea=new JTextArea(1,5);
+        this.ddayJTextArea.setBounds(550,585,100,30);
+        this.ddayJTextArea.setFont(new Font("宋体",Font.PLAIN,25));
         JLabel ddayJLabel=new JLabel("日");
         ddayJLabel.setBounds(675,553,100,100);
         ddayJLabel.setFont(new Font("宋体",Font.BOLD,30));
@@ -181,7 +187,7 @@ class MyPanel_insert extends JPanel{
         this.insertButton=new InsertButton(this.frame);
         this.panel_insert.add(this.insertButton.b_return);
         this.panel_insert.add(this.insertButton.b_submit);
-        
+
         this.panel_insert.add(fatherJLabel);
         this.panel_insert.add(fatherJTextArea);
         this.panel_insert.add(nameJLabel);
@@ -212,6 +218,7 @@ class MyPanel_init extends JPanel{
     InitButton initButton;
     MyFrame frame=null;
     public MyPanel_init(MyFrame frame){
+        super();
         this.frame=frame;
         this.panel_init=new JPanel();
         this.panel_init.setBackground(new Color(173, 216, 230));
@@ -258,7 +265,10 @@ class InsertButton extends JButton{
         this.b_return.setBounds(1400,700,100,50);
         this.b_return.setFont(new Font("宋体",Font.BOLD,30));
         this.b_return.setForeground(Color.gray);
-
+        //绑定事件
+        MyActionListener myActionListener=new MyActionListener(this.frame);
+        this.b_return.addActionListener(myActionListener);
+        this.b_submit.addActionListener(myActionListener);
     }
 }
 
@@ -267,8 +277,8 @@ class MyActionListener implements ActionListener{
     MyFrame frame=null;
     public void actionPerformed(ActionEvent e){
         if(e.getActionCommand()=="插入"){
-            this.frame.container.remove(this.frame.panel_init);
-            this.frame.container.add(this.frame.panel_insert);
+            this.frame.container.remove(this.frame.myPanel_init.panel_init);
+            this.frame.container.add(this.frame.myPanel_insert.panel_insert);
             this.frame.container.revalidate();
             this.frame.container.repaint();
         }
@@ -279,7 +289,33 @@ class MyActionListener implements ActionListener{
             System.out.println("789");
         }
         else if(e.getActionCommand()=="提交"){
+            long T=0;
+            boolean alive;
+            int dday=-1,dmonth=-1,dyear=-1;
+            JavaGUI ctj=new JavaGUI();
+
+            String father=this.frame.myPanel_insert.fatherJTextArea.getText();
+            String name=this.frame.myPanel_insert.nameJTextArea.getText();
+            int byear=Integer.parseInt(this.frame.myPanel_insert.byearJTextArea.getText());
+            int bmonth=Integer.parseInt(this.frame.myPanel_insert.bmonthJTextArea.getText());
+            int bday=Integer.parseInt(this.frame.myPanel_insert.bdayJTextArea.getText());
+            String address=this.frame.myPanel_insert.addressJTextArea.getText();
             
+            if(this.frame.myPanel_insert.aliveButtonGroup.getSelection()==this.frame.myPanel_insert.aliveYesJRadioButton.getModel()){
+                alive=true;
+            }
+            else{
+                alive=false;
+                dyear=Integer.parseInt(this.frame.myPanel_insert.dyearJTextArea.getText());
+                dmonth=Integer.parseInt(this.frame.myPanel_insert.dmonthJTextArea.getText());
+                dday=Integer.parseInt(this.frame.myPanel_insert.ddayJTextArea.getText());
+            }
+            
+            T=ctj.insert(T, father, name, byear, bmonth, bday, alive, address, alive, dyear, dmonth, dday);
+            this.frame.container.remove(this.frame.myPanel_insert.panel_insert);
+            this.frame.container.add(this.frame.myPanel_init.panel_init);
+            this.frame.container.revalidate();
+            this.frame.container.repaint();
         }
     }
     public MyActionListener(MyFrame frame){
