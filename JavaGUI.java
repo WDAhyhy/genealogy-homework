7b6c7893dc9380161136f0f2427c5e1d154d8c95
WDAhyhy <142432507+WDAhyhy@users.noreply.github.com>
2024-06-09 11:57:11 +0800

    update

--- JavaGUI.java
+++ JavaGUI.java
@@ -1023,7 +1023,8 @@ class MyActionListener implements ActionListener{
         else if(e.getActionCommand()=="排序"){
             JavaGUI ctj=new JavaGUI();
             this.frame.myPanel_sortByBirth.sortByBirthJLabel.setText(ctj.sortByBirth(this.frame.T));
-            this.frame.myPanel_sortByBirth.sortByBirthJLabel.setFont(new Font("宋体",Font.BOLD,1900/(this.frame.myPanel_sortByBirth.sortByBirthJLabel.getText().length())));
+            this.frame.myPanel_sortByBirth.sortByBirthJLabel.setBounds(20,20,this.frame.myPanel_sortByBirth.panel_sortByBirth.getWidth(),this.frame.myPanel_sortByBirth.panel_sortByBirth.getHeight());
+            this.frame.myPanel_sortByBirth.sortByBirthJLabel.setFont(new Font("宋体",Font.BOLD,(this.frame.myPanel_sortByBirth.panel_sortByBirth.getWidth()+300)/(this.frame.myPanel_sortByBirth.sortByBirthJLabel.getText().length())));
             this.frame.container.removeAll();
             this.frame.container.add(this.frame.myPanel_sortByBirth.panel_sortByBirth);
             this.frame.container.revalidate();
